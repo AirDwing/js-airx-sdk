51bbe5c56ab2732b6890aad1b2578d753296272d
Willin Wang <willin@willin.org>
2017-10-10 09:54:24 +0800

    add browser support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const request = require('./request');
-const uploader = require('./uploader');
-
-const { hmac } = require('@dwing/common');
+const axios = require('axios');
+const Base64 = require('crypto-js/enc-base64');
+const hmacSHA1 = require('crypto-js/hmac-sha1');
+const hmacSHA256 = require('crypto-js/hmac-sha256');
 const debug = require('debug')('@airx/sdk');
+
+const hmac = { sha1: hmacSHA1, sha256: hmacSHA256 };
 
 const DEFAULTS = {
   SecretId: '',
@@ -14,14 +16,17 @@ const DEFAULTS = {
 class SDK {
   constructor(options) {
     this.options = Object.assign({}, DEFAULTS, options);
-    this.uploader = uploader;
+    axios.defaults.timeout = 5000;
+    axios.defaults.baseURL = `http${this.options.Secure ? 's' : ''}://${this.options.Domain}`;
   }
   getSignature(params, opts) {
     const toCheck = Object.keys(params).sort()
     .map(key => `${(key.indexOf('_') ? key.replace(/_/g, '.') : key)}=${params[key]}`).join('&');
-    const signature = hmac(`${opts.method}${this.options.Domain}${opts.url}?${toCheck}`,
-      this.options.SignatureMethod === 'HmacSHA256' ? 'sha256' : 'sha1', this.options.SecretKey);
+    const signature = Base64.stringify(hmac[
+      this.options.SignatureMethod === 'HmacSHA256' ? 'sha256' : 'sha1'
+    ](`${opts.method}${this.options.Domain}${opts.url}?${toCheck}`, this.options.SecretKey));
     debug(`${opts.method}${this.options.Domain}${opts.url}?${toCheck}`);
+    debug(signature);
     return signature;
   }
   request(data, opts) {
@@ -33,28 +38,25 @@ class SDK {
     params.Signature = this.getSignature(params, opts);
     debug(params);
     if (opts.method === 'GET') {
-      opts.qs = params;
-    } else {
-      opts.form = params;
-      opts.headers = [
-        {
-          name: 'content-type',
-          value: 'application/x-www-from-urlencoded'
-        }
-      ];
+      return axios.get(opts.url, {
+        params
+      });
     }
-    opts.timeout = 5000;
-    opts.url = `http${this.options.Secure ? 's' : ''}://${this.options.Domain}${opts.url}`;
-    return request(opts);
+    return axios.post(opts.url, {
+      data: params
+    });
   }
   get(url, data) {
-    return this.request(data, {method: 'GET', url});
+    return this.request(data, { method: 'GET', url });
   }
   post(url, data) {
-    return this.request(data, {method: 'POST', url});
+    return this.request(data, { method: 'POST', url });
   }
+  // eslint-disable-next-line class-methods-use-this
   upload(data, url) {
-    return this.uploader(data, { method: 'POST', url });
+    return axios.post(url, {
+      data
+    });
   }
 }
 
